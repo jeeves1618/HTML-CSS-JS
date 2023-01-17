0c6d2cf5e5b3e9bf855d7f51f01adb757262ed14
jeeves1618 <jeeves1618@gmail.com>
2023-01-17 23:04:07 +0530

    JS Functions

---
+++ 22 Ajax/fetch.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(function (ajaxResponse) {
+      console.log(ajaxResponse);
+      //ajaxResponse can't be read since it is a readstream.
+      //So, you have to use a json method, which itself is an async function returning a promisr
+      return ajaxResponse.json();
+    })
+    .then(function (formatedData) {
+      console.log(formatedData);
+    });
+};
+
+getCountryData("portugal");
